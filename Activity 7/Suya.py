13b39261c7834879ff22eb8f63bdf6f9970b7e9e
suyachristian <christiansuya00@gmail.com>
2024-09-29 15:21:01 +0800

    Suya.py

---
+++ Activity 7/Suya.py
@@ -0,0 +1,25 @@
+print()
+print("\t YEARS OF SERVICE IN PYTHON");
+print()
+years = int(input("Enter years of service: "))
+office = str(input("Enter office: "))
+
+if years >= 10:
+   if office.upper() == "IT":
+       print(f"Amount Given: 10000")
+   elif office.upper() == "ACCT":
+       print(f"Amount Given: 12000")
+   elif office.upper() == "HR":
+       print(f"Amount Given: 15000")
+elif years < 10:
+    if office.upper() == "IT":
+       print (f"Amount Given: 5000")
+    elif office.upper() == "ACCT":
+        print (f"Amount Given: 6000")
+    elif office.upper() == "HR":
+        print (f"Amount Given: 7500")
+        
+print()
+print("\tEnd OF Program");
+print()
+       
